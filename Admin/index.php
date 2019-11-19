5b442acace785b2b264b8ee1b43ab348cfd83a38
PEDRO MARTIN ESCOBAR GOMEZ <pedro-escobar@javeriana.edu.co>
2019-11-19 17:46:02 -0500

    Carpetas nuevas

---
+++ Admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Página principal del proyecto</title>
+	</head>
+	<body>
+		<?php		
+            echo '<h1>Ventana principal</h1>';
+            echo '<a href=http://localhost/admin.php> Administra los créditos </a><br>';
+			echo '<a href=http://localhost/finDeMes.php> Operaciones de Fin de Mes </a><br>';
+			echo '<a href=http://localhost/logout.php> Salir </a><br>';
+		?>		
+	</body>
+</html>
+<?php
+	
+?>
---
+++ Cliente/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Página principal del proyecto</title>
+	</head>
+	<body>
+		<?php		
+            echo '<h1>Ventana principal</h1>';
+            echo '<a href=http://localhost/retiro.php> Retira tus JaveCoins </a><br>';
+			echo '<a href=http://localhost/comprar.php> Realiza una compra </a><br>';
+			echo '<a href=http://localhost/consignar.php> Consigna dinero a una cuenta </a><br>';
+		?>		
+	</body>
+</html>
+<?php
+	
+?>
