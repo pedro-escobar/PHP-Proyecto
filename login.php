8a29906c34305504b2da4240ce9363153901f269
juan_triana <juan_triana@javeriana.edu.co>
2019-11-19 18:54:08 -0500

    Login y registro ya

---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCRYPE HTML>
+<HEAD>
+    <title>Login</title>
+</HEAD>
+<BODY>
+    <?php
+        echo "<h1> Login </h1>";
+        session_start();
+        $_SESSION['id'] = null;
+        $_SESSION['nomusuario'] = null;
+        $_SESSION['rol'] = null;
+        include_once dirname(__FILE__) . '/Utils/config.php';
+        $con=mysqli_connect(HOST_DB,USUARIO_DB,USUARIO_PASS,NOMBRE_DB);
+        // Verificar conexión
+        if (mysqli_connect_errno())
+        {
+            echo "Error en la conexión: ". mysqli_connect_error();
+        }else{
+            $val="";
+            $cadena = "";
+            $cadena .= '<form action="" method="post">
+                Nombre usuario: <input type="text" name="nombre" value=\'';
+                if (isset($_POST["nombre"])){ 
+                    $val = $_POST["nombre"];
+                }
+                $cadena .= $val.'\'><br>
+                Contraseña: <input type="password" name="contrasena" value=\'';
+                if (isset($_POST["contrasena"])){ 
+                    $val = $_POST["contrasena"];
+                }
+                $cadena .= $val.'\'><br>
+                <input type="submit" value="Iniciar sesion" name = sesion>
+                <br>
+                <a href="http://localhost/PHP-Proyecto/registro.php">Registrarme</a>
+            </form>';
+            echo $cadena;
+            if (isset($_POST['sesion'])) {
+                if (isset($_POST['nombre']) && isset($_POST['contrasena'])) {
+                    $sql = "SELECT * FROM usuarios WHERE username='".$_POST['nombre']."'";
+                    $resultado = mysqli_query($con,$sql);
+                    if(mysqli_num_rows($resultado) > 0){
+                        $row = mysqli_fetch_assoc($resultado);
+                        $contrasenaa = $row['password'];
+                        //echo $contrasenaa;
+                        //echo $_POST['contrasena'];
+                        if (password_verify($_POST['contrasena'], $contrasenaa)) {
+                            // Success!
+                            echo "Inicio sesion correctamente";
+                            $rol = $row['rol'];
+                            echo $rol;
+                            if ($rol == 'admin'){
+                                //echo "admin";
+                                $_SESSION['id'] = null;
+                                $_SESSION['nomusuario'] = null;
+                                $_SESSION['rol'] = $rol;
+                                header("Location: /PHP-Proyecto/Admin/index.php");
+                            }
+                            if ($rol == 'cliente'){
+                                //echo "usuario";
+                                $_SESSION['id'] = $row['id'];
+                                $_SESSION['nomusuario'] = $row['username'];
+                                $_SESSION['rol'] = $rol;
+                                header("Location: /PHP-Proyecto/Cliente/index.php");
+                            }
+                        }
+                        else {
+                            // Invalid credentials
+                            echo "La contraseña no es correcta";
+                        }
+                    }
+                    else{
+                        echo "El nombre de usuario no existe";
+                    }
+                }
+            }
+        }
+    ?>
+</BODY>
---
+++ registro.php
@@ -0,0 +1,64 @@
+<!DOCRYPE HTML>
+<HEAD>
+    <title>Registro</title>
+</HEAD>
+<BODY>
+    <?php
+        echo "<h1> Registro </h1>";
+        include_once dirname(__FILE__) . '/Utils/config.php';
+        $con=mysqli_connect(HOST_DB,USUARIO_DB,USUARIO_PASS,NOMBRE_DB);
+        // Verificar conexión
+        if (mysqli_connect_errno())
+        {
+            echo "Error en la conexión: ". mysqli_connect_error();
+        }else{
+            $val="";
+            $cadena = "";
+            $cadena .= '<form action="" method="post">
+                Nombre usuario: <input type="text" name="nombre"><br>
+                Contraseña: <input type="password" name="contrasena"><br>
+                <input type="submit" value="Registrarse" name = registro>
+            </form>';
+            echo $cadena;
+            if (isset($_POST['registro'])) {
+                if (isset($_POST['nombre']) && isset($_POST['contrasena'])) {
+                    $sql = "SELECT * FROM usuarios WHERE username='".$_POST['nombre']."'";
+                    //echo $sql;
+                    $resultado = mysqli_query($con,$sql);
+                    if(mysqli_num_rows($resultado) < 1){
+                        $sql = 'SELECT * FROM usuarios';
+                        $resultado = mysqli_query($con,$sql);
+                        if(mysqli_num_rows($resultado) > 0){
+                            $hash = password_hash($_POST['contrasena'], PASSWORD_DEFAULT);
+                            //echo $hash;
+                            $sql = 'INSERT INTO '."usuarios"." (username ,password, rol) VALUES ('".$_POST['nombre']."','".$hash."','cliente')";
+                            //echo $sql;
+                            if(mysqli_query($con,$sql)){
+                                echo "Cliente creado correctamente";
+                                echo '<br><a href="http://localhost/PHP-Proyecto/login.php">Iniciar sesion</a>';
+                            }
+                            else{
+                                echo "Error creando Cliente".mysqli_error($con);
+                            }
+                        }
+                        else{
+                            $hash = password_hash($_POST['contrasena'], PASSWORD_DEFAULT);
+                            $sql = 'INSERT INTO '."usuarios"." (username ,password, rol) VALUES ('".$_POST['nombre']."','".$hash."','"."admin"."')";
+                            //echo $sql;
+                            if(mysqli_query($con,$sql)){
+                                echo "Admin creado correctamente";
+                                echo '<br><a href="http://localhost/PHP-Proyecto/login.php">Iniciar sesion</a>';
+                            }
+                            else{
+                                echo "Error creando admin".mysqli_error($con);
+                            }
+                        }
+                    }
+                    else{
+                        echo "El nombre de usuario ya existe";
+                    }
+                }
+            }
+        }
+    ?>
+</BODY>
